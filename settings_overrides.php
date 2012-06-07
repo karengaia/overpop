a672fef2b57eb12f16926d808b10d185f756e924
Moxley Stratton <moxley@moxleydata.com>
2012-06-06 22:45:52 -0700

    Remove password files

--- settings_overrides.php
+++
@@ -1,5 +0,0 @@
-<?php
-$CONFIG['mysql_host']     = 'db.telavant.com';
-$CONFIG['mysql_user']     = 'overpop';
-$CONFIG['mysql_password'] = '';
-$CONFIG['mysql_name']     = 'overpop';
--- settings_overrides.pl
+++
@@ -1,7 +0,0 @@
-%CONFIG_OVERRIDES = (
-  db_host     => 'db.telana.com',
-  db_user     => 'overpop',
-  db_password => '',
-  db_name     => 'overpop'
-);
-1;
